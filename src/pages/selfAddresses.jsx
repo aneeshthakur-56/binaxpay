b6c48ec782cd4c903fefb5cca5368b309aab3331
anmol741 <anmoljstech@gmail.com>
2025-07-07 19:03:26 +0530

    generate self address

--- src/pages/selfAddresses.jsx
+++ src/pages/selfAddresses.jsx
@@ -8,28 +8,32 @@ const SelfAddresses = () => {
         showWallets()
     }, []);
     const showWallets = async() => {
-         getData('/user/self_address_list' ) .then((res) => {
+            getData('/user/self_address_list' ) .then((res) => {
                     setWallets(res.data.data)
             }) .catch((err) => console.error('ee',err)); 
     }
-      const handleCopy = (text) => {
+    const handleCopy = (text) => {
         navigator.clipboard.writeText(text);
         alert('Address copied to clipboard!');
-    };
-    const handleGoToDetails = () => {
-        const id = 123;
-        // navigate(`/self_transactions/${id}`);
+    }; 
+    const generateAddress = () => {
+        const confirmAction = window.confirm("Are you sure you want to generate Address?");
+        if (confirmAction) {
+            getData('/user/generate_self_address' ) .then((res) => {
+                   showWallets()
+            }) .catch((err) => console.error('ee',err)); 
+        }
     };
     return(
         <div className="container py-5 bg-dark text-white min-vh-100">
             <div className="text-center mb-4">
-                <button className="btn btn-primary rounded-pill px-4 py-2">
+                <button className="btn btn-primary rounded-pill px-4 py-2" onClick={() => generateAddress()}>
                 + Generate New Address
                 </button>
             </div>
             <div className="row g-4">
                 {wallets.map((wallet,index) => (
-                <div key={wallet.id} className="col-md-6 col-lg-4">
+                <div key={wallet._id} className="col-md-6 col-lg-4">
                     <div className="card bg-secondary text-white rounded-4 shadow p-3">
                     <div className="card-body">
                         <h5 className="card-title fw-bold">Wallet {index + 1}</h5>

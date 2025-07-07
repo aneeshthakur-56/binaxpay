8711f413ce7c751246e4460a41dd9ace4fefcf8d
Ajay verma <ajayverma.vit@gmail.com>
2025-07-07 17:05:00 +0530

    dashboard ui fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './component/Navbar';
-import AuthGuard from './component/AuthGuard';
-import HeroSection from './component/HeroSection';
-import StatsSection from './component/StatsSection';
-import FeaturesSection from './component/FeaturesSection';
-import Business from './component/Business';
-import Pricing from './component/Pricing';
-import Testimonials from './component/Testimonials';
-import CallToAction from './component/CallToAction';
-import FaqSection from './component/FaqSection';
-import ContactSection from './component/ContactSection';
-import Footer from './component/Footer';
-import Signin from './component/Signin';
-import Signup from './component/Signup';
-import VerifySignup from './component/verifySignup'
-import VerifySignin from './component/verifySignin.jsx'
-import RegisterSuccess from './component/RegisterSuccess'
-import Forgot from './component/Forgot';
-import Dashboard from './pages/Dashboard'
-import DashboardLayout from './pages/DashboardLayout';
-import Profile from './pages/Profile';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import AuthGuard from "./component/AuthGuard";
+import HeroSection from "./component/HeroSection";
+import StatsSection from "./component/StatsSection";
+import FeaturesSection from "./component/FeaturesSection";
+import Business from "./component/Business";
+import Pricing from "./component/Pricing";
+import Testimonials from "./component/Testimonials";
+import CallToAction from "./component/CallToAction";
+import FaqSection from "./component/FaqSection";
+import ContactSection from "./component/ContactSection";
+import Footer from "./component/Footer";
+import Signin from "./component/Signin";
+import Signup from "./component/Signup";
+import VerifySignup from "./component/verifySignup";
+import VerifySignin from "./component/verifySignin.jsx";
+import RegisterSuccess from "./component/RegisterSuccess";
+import Forgot from "./component/Forgot";
+import Dashboard from "./pages/Dashboard";
+import DashboardLayout from "./pages/DashboardLayout";
+import Profile from "./pages/Profile";
 
 // 👇 Home component without Signin/Signup
 const Home = () => (
@@ -41,14 +41,20 @@ const Home = () => (
 const App = () => (
   <Router>
     <Routes>
- 
-      <Route path="/" element={ <><Navbar /> <Home /></>} />
+      <Route
+        path="/"
+        element={
+          <>
+            <Navbar /> <Home />
+          </>
+        }
+      />
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/VerifySignup" element={<VerifySignup />} />
-      <Route path="/verify-signin" element={<VerifySignin />} /> 
+      <Route path="/verify-signin" element={<VerifySignin />} />
 
-      <Route path="/register-success" element={<RegisterSuccess/>}/>
+      <Route path="/register-success" element={<RegisterSuccess />} />
       <Route path="/forgot" element={<Forgot />} />
       <Route element={<AuthGuard />}>
         <Route element={<DashboardLayout />}>
@@ -57,7 +63,6 @@ const App = () => (
         </Route>
       </Route>
     </Routes>
-
   </Router>
 );
 
